34bd7567d29b189616e02f8e14f0dc5fe81d22b7
Akshaya Niraula <xniraula@live.com>
2020-05-31 14:33:10 -0600

    initial commit

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,35 @@ namespace ExternalToolsArgsDecipherer
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1(string[] args)
         {
             InitializeComponent();
+
+            Text = "OpenFlows External Tools Arguments Viewer";
+            Icon = IconExtractor.Extract("shell32.dll", 209, false);
+            Args = args;
         }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            if (Args.Length <= 0)
+                return;
+                        
+            Args = Args[0].Split('|');
+
+            for (int i = 0; i < Args.Length; i++)
+            {
+                var message = $"{i}: {Args[i]} {Environment.NewLine}";
+
+                textBox.AppendText( message);
+                Console.WriteLine(message);
+            }
+
+
+            base.OnLoad(e);
+        }
+
+
+        private string[] Args { get; set; }
     }
 }
